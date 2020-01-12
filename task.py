e0dff817bd4a01b0e71b86fa84296ba165d76246
Dmitry Shuran <dmitry.shuran@gmail.com>
2020-01-12 20:17:00 +0300

    Решил задачу

--- task.py
+++ task.py
@@ -1,3 +1,51 @@
+import time
+
+
+def get_cell_changes(a, n, cell_i, cell_j):
+    neigh_fish = 0
+    neigh_shrimps = 0
+    for di in range(-1, 2):
+        for dj in range(-1, 2):
+            new_i = cell_i + di
+            new_j = cell_j + dj
+            if 0 <= new_i < n and 0 <= new_j < n:
+                # print('new i = ', new_i, ' new j = ', new_j, ' n = ', n)
+                if a[new_i][new_j] == 's':
+                    neigh_shrimps = neigh_shrimps + 1
+                elif a[new_i][new_j] == 'f':
+                    neigh_fish = neigh_fish + 1
+    if a[cell_i][cell_j] == 'f':
+        if neigh_fish == 2 or neigh_fish == 3:
+            return 'f'
+        else:
+            return 'e'
+    elif a[cell_i][cell_j] == 's':
+        if neigh_shrimps == 2 or neigh_shrimps == 3:
+            return 's'
+        else:
+            return 'e'
+    elif a[cell_i][cell_j] == 'e':
+        if neigh_fish == 3:
+            return 'f'
+        elif neigh_shrimps == 3:
+            return 's'
+        else:
+            return 'e'
+    else:
+        return 'r'
+
+
+def get_new_world(a, n):
+    # Creating new array for the next world
+    next_world = [0] * n
+    for i in range(n):
+        next_world[i] = [0] * n
+    # Changing every cell
+    for i in range(n):
+        for j in range(n):
+            next_world[i][j] = get_cell_changes(a, n, i, j)
+    return next_world
+
 
 # s - креветка, f - рыба, r - скала, e - пустое место
 def main():
@@ -5,16 +53,22 @@ def main():
     n = int(input())
     a = []
     for i in range(n):
-        row = input().split()
-        a.append(row)
-    #
+        row = input()
+        row_list = list(row)
+        a.append(row_list)
+    print(a)
+    # Scenario
+    while True:
+        a = get_new_world(a, n)
+        # print('new a = ', a)
+        # Output
+        for i in range(n):
+            for j in range(n):
+                print(a[i][j], end='')
+            print('\n', end='')
+        time.sleep(1)
+        print('------------------------\n', end='')
 
-
-    # Output
-    for i in range(n):
-        for elem in a[i]:
-            print(elem, end='')
-        print('\n', end='')
 
 if __name__ == '__main__':
     main()
